b3648419bc629250c34446e55b6831334839d9db
JonatanHN <jonapato00@gmail.com>
2022-02-22 12:42:48 -0600

    two commit

--- api/letters/[id].ts
+++ api/letters/[id].ts
@@ -4,16 +4,22 @@ const users = [{
     id: 1,
     name: "John Doe",
     email: "johndoe@gmail.com",
-    age: 27
+    age: 27,
+    body: "Hello World",
+    image: "https://avatars0.githubusercontent.com/u/24394905?s=460&u=e8d9c8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f&v=4"
 },
 {
     id: 2,
     name: "Jane Doe",
     email: "janedoe@gmail.com",
-    age: 23
+    age: 23,
+    body: "Hello World",
+    image: "https://avatars0.githubusercontent.com/u/24394905?s=460&u=e8d9c8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f&v=4"
 }];
 
 export default (req: NowRequest, res: NowResponse) => {
 	const { id } = req.query;
 	res.status(200).json({ user: users[Number(id)] });
 };
+
+
